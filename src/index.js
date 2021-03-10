dc20adae1a962ce7314860c234dd77c23f03ef3d
Pavel Popov <inkvitop@gmail.com>
2021-03-10 04:21:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+      let reversNumber;
+      let reversNumberTemp;
+      let reversNumberToNumber;
+      reversNumber = n.toString().split('').reverse().join('');
+      if (n < 0) {
+            reversNumberTemp = n * -1;
+            reversNumberTemp = reversNumberTemp.toString().split('').reverse().join('');
+            reversNumberToNumber = Number(reversNumberTemp)
+            return reversNumberToNumber;
+      }else if (reversNumber.substr(0,1) == 0) {
+            reversNumberToNumber = Number(reversNumber.slice(1))
+            return reversNumberToNumber;
+      } else {
+            reversNumber.toString().split('').reverse().join('');
+            reversNumberToNumber = Number(reversNumber)
+            return reversNumberToNumber;
+      }
 }
